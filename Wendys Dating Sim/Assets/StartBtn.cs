f6e55859759590c09ca7129112f75aad3c3a4109
tager1199 <32684792+tager1199@users.noreply.github.com>
2019-10-19 15:48:20 +0100

    Renpy Game
    Changed from using unity to renpy

---
+++ Wendys Dating Sim/Assets/StartBtn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        //btn.GetComponentInChildren<Text>().text = "Start";
+    }
+
+    void TaskOnClick()
+    {
+        string textCheck = gameObject.GetComponent<Button>().name;
+
+        switch (textCheck)
+        {
+
+            case "Start":
+                SceneManager.UnloadSceneAsync("Main Menu");
+                SceneManager.LoadScene("Start");
+                break;
+            case "Quit":
+                Application.Quit();
+                break;
+            default:
+                break;
+
+
+        }
+    }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+}
